d8083e1e05c6f20147c5326ca8c34e12456bf342
Jaden See <jadenas2028@student.cis.edu.hk>
2025-09-14 23:48:35 +0800

    Jaden 14/9/2025

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        int userAge;
+        int eligibleAge;
+
+        Scanner age = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        userAge = age.nextInt();
+
+        eligibleAge=(userAge/2)+7;
+
+        System.out.println(userAge + "-year olds should date somebody who is at least " + eligibleAge + " years old.");
+        age.close();
+
     }
 }
