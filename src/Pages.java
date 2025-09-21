e1594433c4b61adf9b519b2ccbdfed61fb6a207c
Jaden See <jadenas2028@student.cis.edu.hk>
2025-09-21 23:10:32 +0800

    Jaden 21/9/2025

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        int age;
+        int giveupAge;
 
+        Scanner userAge = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        age = userAge.nextInt();
+
+        giveupAge = 100 - age;
+
+        System.out.println(age + "-year olds should read at least " + giveupAge + " pages before giving up on a book.");
+        userAge.close();
     }
 }
